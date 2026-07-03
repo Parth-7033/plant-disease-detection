ccc7453acde9901e4f32fe19c21ba9757dff367f
Parth-7033 <120300104+Parth-7033@users.noreply.github.com>
2026-07-03 16:08:18 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -37,5 +37,9 @@ async def predict(
     }
 
 
+
+import os
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
